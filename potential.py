50a3e951d42c0034b5bce688a01aef7b8d82b225
Dorian Yeh <56494806+civond@users.noreply.github.com>
2023-04-24 15:12:26 -0400

    Updated potential.py

--- potential.py
+++ potential.py
@@ -1,30 +1,36 @@
 import serial
+import serial.tools.list_ports
 import time
 import csv
 
-# make sure the 'COM#' is set according the Windows Device Manager
-ser = serial.Serial('COM9', 9800, timeout=1)
-#time.sleep(2)
+# BE-GY 6503 Bioinstrumentation Term Project: Realtime ECG monitoring with AD8232/ESP32
+# Contributors: Dorian Yeh, Lokesh Sharma, Nanzhong Deng, Neil Sequira
 
+ports = serial.tools.list_ports.comports()
+for port, desc, hwid in sorted(ports):
+    print("{}: {} [{}]".format(port, desc, hwid))
+
+ser = serial.Serial(port, 9800, timeout=1)
+
+# Create .csv file
 fileName = 'analog-data.csv'
 file = open(fileName, "a")
 print("Created .csv file")
 
-sensorData = []
+sensorData = []  # list for data
 
+# Gather data from serial monitor (arduino)
 for i in range(10000):
-    line = ser.readline()   # read a byte
-    #print(line)
+    line = ser.readline()  # read a byte
     if line:
         string = line.decode()  # convert the byte string to a unicode string
-        num = int(string) # convert the unicode string to an int
-        #print(num)
-        sensorData.append(num)
+        data = int(string)  # convert the unicode string to an int
+        sensorData.append(data)
 ser.close()
-
 print(sensorData)
+
+# Write to .csv
 with open(fileName, 'w') as f:
     write = csv.writer(f)
     write.writerow(sensorData)
-print('Finished Writing')
-# create the CSV
+print('Finished Writing Data to .csv')
