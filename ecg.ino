c4606fc281a0af5bb4092049776dd2d9ba9d1719
Dorian Yeh <56494806+civond@users.noreply.github.com>
2023-05-01 14:10:06 -0400

    Add files via upload

--- ecg.ino
+++ ecg.ino
@@ -1,16 +1,22 @@
 //BE-GY 6503 Final Project: Realtime ECG Acquisition (AD8232 - ESP32) 
 
-const int lo1 = 16; //D16
-const int lo2 = 4; //D4
+//Parameters
+const int lo1 = 3; //D16
+const int lo2 = 2; //D4
 
 int lo1_state=0;
 int lo2_state=0;
 
-int output = 2; //D2
+int output = A0; //D2 or D4
 
+int freq = 5; //data collection frequency ~x milliseconds
+int baud = 9600; //baud rate
+int data;
+
+// Implementation
 void setup() {
   // initialize the serial communication:
-  Serial.begin(9600);
+  Serial.begin(baud);
   pinMode(lo1, INPUT); // Setup for leads off detection LO + 
   pinMode(lo2, INPUT); // Setup for leads off detection LO -
   lo1_state = digitalRead(lo1);
@@ -18,14 +24,15 @@ void setup() {
 }
 
 void loop() {
-  
   if((lo1_state == 1)||(lo2_state == 1)){
     Serial.println('!');
   }
   else{
     // send the value of analog input 0:
       Serial.println(analogRead(output));
+      data = analogRead(output);
   }
   //Wait for a bit to keep serial data from saturating
-  delay(1);
+  delay(freq); // adjust this
 }
+
